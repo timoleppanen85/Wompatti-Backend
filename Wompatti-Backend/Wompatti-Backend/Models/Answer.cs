b9fc9ae3f7a94eea57c212aad9f9f3d27bfeab16
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2019-10-24 19:29:51 +0300

    Reverse engineering from DB
    +gitignore appsettings.json

---
+++ Wompatti-Backend/Wompatti-Backend/Models/Answer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wompatti_Backend.Models
+{
+    public partial class Answer
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column("Answer")]
+        public string Answer1 { get; set; }
+        public string ExtraTest { get; set; }
+        public int AnswerSet { get; set; }
+        public long QuestionId { get; set; }
+        public long UserId { get; set; }
+
+        [ForeignKey("QuestionId")]
+        [InverseProperty("Answer")]
+        public virtual Question Question { get; set; }
+        [ForeignKey("UserId")]
+        [InverseProperty("Answer")]
+        public virtual User User { get; set; }
+    }
+}
---
+++ Wompatti-Backend/Wompatti-Backend/Models/WompattidbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Wompatti_Backend.Models
+{
+    public partial class WompattidbContext : DbContext
+    {
+        public WompattidbContext()
+        {
+        }
+
+        public WompattidbContext(DbContextOptions<WompattidbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admin> Admin { get; set; }
+        public virtual DbSet<Answer> Answer { get; set; }
+        public virtual DbSet<Question> Question { get; set; }
+        public virtual DbSet<Subject> Subject { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Answer>(entity =>
+            {
+                entity.HasOne(d => d.Question)
+                    .WithMany(p => p.Answer)
+                    .HasForeignKey(d => d.QuestionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Answer_Question");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Answer)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK_Answer_User");
+            });
+
+            modelBuilder.Entity<Question>(entity =>
+            {
+                entity.Property(e => e.Visible).HasDefaultValueSql("((1))");
+
+                entity.HasOne(d => d.Subject)
+                    .WithMany(p => p.Question)
+                    .HasForeignKey(d => d.SubjectId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Question_Subject");
+            });
+
+            modelBuilder.Entity<Subject>(entity =>
+            {
+                entity.Property(e => e.Visible).HasDefaultValueSql("((1))");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
